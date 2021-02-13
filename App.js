86624b3d37883d15d0370fb9a0f3629c016a2f05
anshika4 <72254585+anshika4@users.noreply.github.com>
2021-02-13 16:20:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import Scanscreen from './screens/Scanscreen';
-import { createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 export default class App extends React.Component {
 render(){
 return (
-<AppContainer/>
+<Scanscreen/>
 );
 }
 }
 
-const TabNavigator=createBottomTabNavigator({
-Scan:{screen:Scanscreen},
-})
-const AppContainer=createAppContainer(TabNavigator)
+
 
 
